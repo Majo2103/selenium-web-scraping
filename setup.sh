6885f66d47fa3922bb77b27fea650978e78d6169
mellamanelpoeta <gerarm.guerrero@gmail.com>
2023-11-25 23:54:17 -0600

    more changes

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@
 VENV_NAME="web-scraping-env"
 
 # Crear y activar el entorno virtual
-python -m venv "$VENV_NAME"
+python3 -m venv "$VENV_NAME"
 source "$VENV_NAME/bin/activate"
 
-# Instalar Selenium 4.9.0
-pip install selenium==4.9.0
+# Instalar requirements.txt en el entorno virtual
+pip install -r requirements.txt
 
 echo "Entorno virtual configurado. Para activar, ejecuta: source $VENV_NAME/bin/activate"
